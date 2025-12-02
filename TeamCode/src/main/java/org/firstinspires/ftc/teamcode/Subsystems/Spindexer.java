c7dcaaf97413609b6afa73162c102630f5025bd5
Xhaemel <xhaemel@gmail.com>
2025-12-01 21:28:48 -0800

    commit Spindexer concept class with the basic logic using a new class instead of Hashmap <color, int>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Spindexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Spindexer.java
@@ -5,11 +5,14 @@ import static org.firstinspires.ftc.teamcode.Subsystems.Spindexer.color.GREEN;
 import static org.firstinspires.ftc.teamcode.Subsystems.Spindexer.color.PURPLE;
 import static org.firstinspires.ftc.teamcode.Subsystems.Spindexer.color.UNDECTED;
 
+import androidx.annotation.NonNull;
+
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.Configs.Config;
+import org.firstinspires.ftc.teamcode.Retired.SpindexerState;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -20,15 +23,10 @@ public class Spindexer {
     private ColorSensor slot0;
     private ColorSensor slot1;
     private ColorSensor slot2;
+    public SpindexerState spindexerState;
     public enum color{
         PURPLE, GREEN, UNDECTED
     }
-    public enum motif{
-        PPG, PGP, GPP
-    }
-
-
-
     Config config;
     ///          |
     ///          |
@@ -51,7 +49,7 @@ public class Spindexer {
         slot0.enableLed(true);
         slot1.enableLed(true);
         slot2.enableLed(true);
-
+        spindexerState = new SpindexerState();
     }
     /**
      * spins the spindexer to a slot
@@ -71,7 +69,6 @@ public class Spindexer {
             portServo.setPosition(1-config.slot2ServoPosition);
         }
         else{
-
         }
     }
     /**
@@ -131,63 +128,62 @@ public class Spindexer {
     public boolean slotIsFull(int givenSlot){
         return readSlotColor(givenSlot) != color.UNDECTED;
     }
-
     /**
      * returns the ideal routing of slots to get pattern/motif
      * @param givenFirstBall first ball color in the motif
      * @param givenSecondBall second ball color in motif
      * @param givenThirdBall etc.
      * @return int [] of the slots
-     *
      */
-    public int[] determineBestSpinRoute(color givenFirstBall, color givenSecondBall, color givenThirdBall){
+    public int[] determineBestSpinRoute(@NonNull color givenFirstBall, @NonNull color givenSecondBall, @NonNull color givenThirdBall){
         int[] order = new int[2];
-        HashMap <color,Integer> slotColors = new HashMap();
+        spindexerState.setSlotState(0, readSlotColor(0));
+        spindexerState.setSlotState(1, readSlotColor(1));
+        spindexerState.setSlotState(2, readSlotColor(2));
 
-        slotColors.put(readSlotColor(0), 0);
-        slotColors.put(readSlotColor(1), 1);
-        slotColors.put(readSlotColor(2), 2);
        //check if the sorter contains the wanted color
-            if(slotColors.containsKey(givenFirstBall)) {
-                //check the hashmap for color
-                order[0] = slotColors.get(givenFirstBall);
-                //delete that color from the hasmap
-                slotColors.remove(order[0], givenFirstBall);
+            if(spindexerState.containsColorInSpindexer(givenFirstBall)) {
+                //check the sorter for color
+                order[0] = spindexerState.getSlotFromColor(givenFirstBall);
+                spindexerState.setSlotState(order[0], UNDECTED);
             }
             //if the sorter doesn't contain the wanted color then use opposite color ball
-            else if(slotColors.containsKey(oppositeBall(givenFirstBall))){
-                //check the hashmap for color
-                order[0] = slotColors.get(oppositeBall(givenFirstBall));
-                //delete that color from the hasmap
-                slotColors.remove(order[0], oppositeBall(givenFirstBall));
+            else if(spindexerState.containsColorInSpindexer(oppositeBall(givenFirstBall))){
+                //check the sorter for color
+                order[0] = spindexerState.getSlotFromColor(oppositeBall(givenFirstBall));
+                spindexerState.setSlotState(order[0], UNDECTED);
             }
             //second ball
-            if(slotColors.containsKey(givenSecondBall)) {
-                order[1] = slotColors.get(givenSecondBall);
-                slotColors.remove(order[1], givenSecondBall);
+            if(spindexerState.containsColorInSpindexer(givenSecondBall)) {
+                //check the sorter for color
+                order[1] = spindexerState.getSlotFromColor(givenSecondBall);
+                spindexerState.setSlotState(order[1], UNDECTED);
             }
             //if the sorter doesn't contain the wanted color then use opposite color ball
-            else if(slotColors.containsKey(oppositeBall(givenSecondBall))) {
-                order[1] = slotColors.get(oppositeBall(givenSecondBall));
-                slotColors.remove(order[1], oppositeBall(givenSecondBall));
+            else if(spindexerState.containsColorInSpindexer(oppositeBall(givenFirstBall))){
+                //check the sorter for color
+                order[1] = spindexerState.getSlotFromColor(oppositeBall(givenFirstBall));
+                spindexerState.setSlotState(order[1], UNDECTED);
             }
             //add safe for if color is undetected
             else{
                 order[1] = Integer.parseInt(null);
             }
             //last ball
-            if(slotColors.containsKey(givenThirdBall)) {
-                order[1] = slotColors.get(givenThirdBall);
-                slotColors.remove(order[1], givenSecondBall);
+            if(spindexerState.containsColorInSpindexer(givenThirdBall)) {
+             //check the sorter for color
+                 order[2] = spindexerState.getSlotFromColor(givenThirdBall);
+                spindexerState.setSlotState(order[2], UNDECTED);
             }
             //if the sorter doesn't contain the wanted color then use opposite color ball
-            else if(slotColors.containsKey(oppositeBall(givenSecondBall))) {
-                order[1] = slotColors.get(oppositeBall(givenThirdBall));
-                slotColors.remove(order[1], oppositeBall(givenThirdBall));
+            else if(spindexerState.containsColorInSpindexer(oppositeBall(givenThirdBall))){
+                 //check the sorter for color
+                order[2] = spindexerState.getSlotFromColor(oppositeBall(givenThirdBall));
+                 spindexerState.setSlotState(order[2], UNDECTED);
             }
             //add safe for if color is undetected
             else{
-                order[1] = Integer.parseInt(null);
+                order[2] = Integer.parseInt(null);
             }
             //return oder in slot #'s with int []
         return order;
